49ded77e140da85cecd83a48aaf53b42dd42bad6
zeeshan zahoor <105010948+zeeshanzahoor786@users.noreply.github.com>
2022-08-04 03:00:35 +0200

    Updates

---
+++ precache-manifest.089f158b4d293e252514bd46fa72ee3e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1f9d2166b2862a482b75d5845a84f6d",
+    "url": "/fitnesswebsite/index.html"
+  },
+  {
+    "revision": "3454d61e4d1d11068a81",
+    "url": "/fitnesswebsite/static/css/main.c780bf6f.chunk.css"
+  },
+  {
+    "revision": "85730d8ca70b10b9f032",
+    "url": "/fitnesswebsite/static/js/2.56793058.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fitnesswebsite/static/js/2.56793058.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3454d61e4d1d11068a81",
+    "url": "/fitnesswebsite/static/js/main.d6de5fba.chunk.js"
+  },
+  {
+    "revision": "83c9cb2827fa1779aad6",
+    "url": "/fitnesswebsite/static/js/runtime-main.e60102ac.js"
+  },
+  {
+    "revision": "b6f21244a120a65e8247f3640aa8c8ea",
+    "url": "/fitnesswebsite/static/media/bgVideo.b6f21244.mp4"
+  },
+  {
+    "revision": "558a8275c6eac3fdc43ba760c2480ddd",
+    "url": "/fitnesswebsite/static/media/classFour.558a8275.jpg"
+  },
+  {
+    "revision": "17401abc39bf9b5408b0c49aae1535d4",
+    "url": "/fitnesswebsite/static/media/classOne.17401abc.jpg"
+  },
+  {
+    "revision": "c5162d4d69ce41a174eb4f456500d812",
+    "url": "/fitnesswebsite/static/media/classThree.c5162d4d.jpg"
+  },
+  {
+    "revision": "92e684d5da73e14ef390d0ff620fb8c5",
+    "url": "/fitnesswebsite/static/media/classTwo.92e684d5.jpg"
+  },
+  {
+    "revision": "f042c2ac7fe16211f6c2806e04fdebad",
+    "url": "/fitnesswebsite/static/media/gymOverlayBg.f042c2ac.jpg"
+  },
+  {
+    "revision": "7afa204f6ab579e2ffccf7f63cead7fd",
+    "url": "/fitnesswebsite/static/media/scheduleBg.7afa204f.jpg"
+  },
+  {
+    "revision": "9af6560400ef14efd6bb74bfb2313b74",
+    "url": "/fitnesswebsite/static/media/trainerOne.9af65604.jpg"
+  },
+  {
+    "revision": "7bfc49e84503eed5ae51943d65d3b6a9",
+    "url": "/fitnesswebsite/static/media/trainerThree.7bfc49e8.jpg"
+  },
+  {
+    "revision": "c7a6feb7730a8818c3f4297f33a6896c",
+    "url": "/fitnesswebsite/static/media/trainerTwo.c7a6feb7.jpg"
+  }
+]);

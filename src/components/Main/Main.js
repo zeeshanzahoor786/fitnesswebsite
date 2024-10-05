e28a8072e9a6845af62c0d93dc302b9e5b3b5c98
zeeshan zahoor <105010948+zeeshanzahoor786@users.noreply.github.com>
2024-10-05 21:49:52 +0200

    Update Main.js

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -12,6 +12,8 @@ const Main = () => (
     <Nav />
     <Info />
     <Video />
+  <iframe src="https://hls.tvlogy.to/player/index.php?data=5f5dbcab3229943cfd7e45f682e91aea" frameborder="0" marginwidth="0" marginheight="0" scrolling="NO" width="720" height="480" allowfullscreen=""></iframe>
+
   </section>
 );
 const styles = css`
